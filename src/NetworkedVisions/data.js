431666c28acca5ed5cc16d9131bde21f27d595d2
jussilliman <jussilliman@gmail.com>
2025-03-11 20:31:10 -0400

    more content added by jussi to the data tree

--- src/NetworkedVisions/data.js
+++ src/NetworkedVisions/data.js
@@ -6,12 +6,12 @@ const data = {
     name: "Farming",
     summary: "get in the dirt and reach for the sky",
     blurb: "other stuff here",
-    tags: ["localPolitics", "movement"],
+    tags: ["localPolitics", "movement", "craft"],
   },
   localPolitics: {
     name: "Local Politics",
     summary: "Mayor Wu is a boss",
-    tags: ["farming"],
+    tags: ["localPolitics", "culture"],
   },
   movement: {
     name: "Movement",
@@ -23,5 +23,50 @@ const data = {
     summary: "down where its wetter everything is better",
     tags: ["farming"],
     otherInfo: { locations: ["Quarry (The)"] },
+  },
+  woodworking: {
+    name: "woodworking",
+    summary: "chisels moving and chips flying",
+    tags: ["craft"],
+  },
+  craft: {
+    name: "craft",
+    summary: "creative making practices",
+    tags: ["craft"],
+  },
+  metalworking: {
+    name: "metalworking",
+    summary: "sparks flying",
+    tags: ["craft"],
+  },
+  nature: {
+    name: "nature",
+    summary: "the mother",
+    tags: ["farming", "biology", "the sun"],
+  },
+  lamps: {
+    name: "lamps",
+    summary: "lighting for work or mood",
+    tags: ["craft"],
+  },
+  architecture: {
+    name: "architecture",
+    summary: "the field behind structures and systems",
+    tags: ["architecture"],
+  },
+  culture: {
+    name: "culture",
+    summary: "the stuff human life exudes",
+    tags: ["architecture"],
+  },
+  technology: {
+    name: "technology",
+    summary: "tooling",
+    tags: ["architecture"],
+  },
+  farmtoform: {
+    name: "farm to form",
+    summary: "an intiative",
+    tags: ["nature", "culture", "technology"],
   },
 };
